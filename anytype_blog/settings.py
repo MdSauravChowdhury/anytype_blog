439c7b8761be13fd41918f15993bccff8504e02b
Md SauRav Chowdhury <36702604+MdSauravChowdhury@users.noreply.github.com>
2019-10-19 02:24:44 +0600

    Update settings.py
    Change Web URL

--- anytype_blog/settings.py
+++ anytype_blog/settings.py
@@ -20,12 +20,12 @@ TEMPLATES_DIRS = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dep*=l3rux6%82160m66wof+56!#ok+u3o=50bp(rlxc6t^yco'
+SECRET_KEY = 'dep*=l3rux6%82160m66w54535of+56!#ok+u003o=50bp(rlxc6t^yco'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['anytypeblog.herokuapp.com']
 
 
 # Application definition
